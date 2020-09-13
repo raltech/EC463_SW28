bfdef3fb7a6570fa9185e236891c1f9be73e7d6b
raltech <raltech.dev@gmail.com>
2020-09-13 02:03:46 -0400

    add creating plot function using plotly

--- client/flask_minimal_client.py
+++ client/flask_minimal_client.py
@@ -1,4 +1,14 @@
+# Flask
 from flask import Flask, render_template
+
+# plotly 
+import plotly
+import plotly.graph_objs as go
+
+# other
+import json
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,3 +16,28 @@ app = Flask(__name__)
 @app.route('/<name>')
 def hello_world(name=None):
     return render_template('test.html', name=name)
+
+def create_plot():
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+
+    data = [
+        go.Bar(
+            x=df['x'], # assign x as the dataframe column 'x'
+            y=df['y']
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+@app.route('/login')
+def sso_login():
+    # sso login
+    pass
+
+if __name__ == '__main__':
+    app.run()
