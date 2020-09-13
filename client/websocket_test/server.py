99c37757342f2ae3e4b6dd2fdf877487d3c08417
raltech <raltech.dev@gmail.com>
2020-09-13 01:45:16 -0400

    support ssl connection for security

--- client/websocket_test/server.py
+++ client/websocket_test/server.py
@@ -1,4 +1,6 @@
 import asyncio
+import pathlib
+import ssl
 import websockets
 
 async def hello(websocket, path):
@@ -10,7 +12,13 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "localhost", 8765)
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_cert_chain(localhost_pem)
+
+start_server = websockets.serve(
+    hello, "localhost", 8765, ssl=ssl_context
+)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
