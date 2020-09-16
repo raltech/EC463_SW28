c9c29acffe862300aed6d2da04d142e0b565bb21
raltech <raltech.dev@gmail.com>
2020-09-16 14:08:34 -0400

    easy run

---
+++ client/run.sh
@@ -0,0 +1,10 @@
+export FN_AUTH_REDIRECT_URI=http://127.0.0.1:8040/google/auth
+export FN_BASE_URI=http://127.0.0.1:8040
+export FN_CLIENT_ID=311949798003-4cg1i3hm9okomerp89jvejurdm173eun.apps.googleusercontent.com
+export FN_CLIENT_SECRET=6RiEmSdLW-UPxYNeK2BZzFB8
+
+export FLASK_APP=client.py
+export FLASK_DEBUG=1
+export FN_FLASK_SECRET_KEY=1234
+
+python -m flask run -p 8040
