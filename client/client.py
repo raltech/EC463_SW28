7e091408d29066e0f6356763e74385a8ced00f65
raltech <raltech.dev@gmail.com>
2020-09-16 14:11:25 -0400

    support google sso login

--- client/client.py
+++ client/client.py
@@ -11,15 +11,77 @@ import numpy as np
 import pandas as pd
 import urllib.request
 
-app = Flask(__name__)
+import functools
+import os
 
-API_KEY = 'put your api here'
+from authlib.client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
+
+import google_auth
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("FN_FLASK_SECRET_KEY", default=False)
+
+app.register_blueprint(google_auth.app)
+
+API_KEY = '6f03dc86e0326463b155da387638eb29' # openweathermap
 
 def tocelcius(temp):
     return str(round(float(temp) - 273.16,2))
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
+def welcome():
+    if google_auth.is_logged_in():
+        user_info = google_auth.get_user_info()
+        #     return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
+
+        """
+        Returns page prompting for a zip code or containing weather for a specific zip code
+        :return:
+        """
+        if request.method == 'POST':
+            city = request.form['city']
+        else:
+            city = 'boston'
+
+        # source contain json data from api
+        try:
+            source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+API_KEY).read()
+        except:
+            city = 'boston (default)'
+            source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=boston&appid='+API_KEY).read()
+        # converting json data to dictionary
+
+        list_of_data = json.loads(source)
+
+        # data for variable list_of_data
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "temp_cel": tocelcius(list_of_data['main']['temp']) + 'C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            "cityname":str(city),
+        }
+
+        bar1 = create_plot()
+        bar2 = create_plot()
+        bar3 = create_plot()
+        bar = [bar1, bar2, bar3]
+
+        return render_template('home.html',data=data, len=len(bar), plot=bar, user_info=user_info)
+
+    return render_template('welcome.html')
+    # return 'You are not currently logged in.'
+
+@app.route('/home', methods=['GET','POST'])
 def home():
+    if google_auth.is_logged_in():
+        user_info = google_auth.get_user_info()
+    #     return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
+
     """
     Returns page prompting for a zip code or containing weather for a specific zip code
     :return:
@@ -55,7 +117,7 @@ def home():
     bar3 = create_plot()
     bar = [bar1, bar2, bar3]
 
-    return render_template('home.html',data=data, len=len(bar), plot=bar)
+    return render_template('home.html',data=data, len=len(bar), plot=bar, user_info=user_info)
 
 
 def create_plot():
