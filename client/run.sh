392037a69df8a198511b45c2338f5fc54f8413b8
raltech <raltech.dev@gmail.com>
2020-09-17 02:20:17 -0400

    fix typo

--- client/run.sh
+++ client/run.sh
@@ -1,7 +1,7 @@
 export FN_AUTH_REDIRECT_URI=http://127.0.0.1:8040/google/auth
 export FN_BASE_URI=http://127.0.0.1:8040
-export FN_CLIENT_ID=xxxxx
-export FN_CLIENT_SECRET=xxxxx
+export FN_CLIENT_ID=
+export FN_CLIENT_SECRET=
 
 export FLASK_APP=client/client.py
 export FLASK_DEBUG=1
